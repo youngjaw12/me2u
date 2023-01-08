bca50d29eb1d2b2653d3e97e6b822f9c317db6ef
techyjaw <agahjonathan@gmail.com>
2023-01-08 17:10:25 +0100

    configured home index and migrated ads database

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  root "home#index"
   resources :ads
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
